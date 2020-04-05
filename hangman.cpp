54e2539fc341abec70198c5ddd93465b960c3cd3
Jesse Turner <jesseturner@Jesses-MacBook-Pro.local>
2020-04-05 15:55:57 -0600

    A couple interesting bugs here, both around creating the scoreboard

--- hangman.cpp
+++ hangman.cpp
@@ -9,14 +9,18 @@ int main()
     cout << "Enter your hangman word: "; 
     cin >> guessword;
     cout << "The word is " << guessword.length() << " letters long.\n";
+    string scoreboard;
+    scoreboard = string(guessword.length(), '_');
+    cout << scoreboard << endl;
     for (int i = 0; i <= 3; i++) {
         cout << "Enter a letter to guess: ";
         cin >> letter;
         int found;
         found = guessword.find(letter);
+        scoreboard = scoreboard.replace(found, 1, '_',  letter);
         if (found != std::string::npos)
-        cout << letter << " is in position " << found+1 << " in the word.\n"; // found
-        else 
+        cout << letter << " is in position " << found+1 << " in the word.\n" << scoreboard << endl;
+        else
         cout << letter << " is not in the word\n"; // not found
         }
     cout << "you are out of guesses.\n";
