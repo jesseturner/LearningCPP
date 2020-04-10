4f832fdb4f41f18804b2c597eb3d966d52a88bc2
Jesse Turner <jesseturner@Jesses-MacBook-Pro.local>
2020-04-09 23:21:27 -0600

    The scoreboard works now.

--- hangman.cpp
+++ hangman.cpp
@@ -13,25 +13,25 @@ int main()
     string scoreboard;
     scoreboard = string(guessword.length(), '_');
     cout << scoreboard << endl;
+    
     for (int i = 0; i <= 3; i++) {
         cout << "Enter a letter to guess: ";
         cin >> letter;
         int found;
         found = guessword.find(letter);
-
-        scoreboard = std::regex_replace(guessword, std::regex(letter), "_");
-        cout << scoreboard << endl;
-
-        /*if (found != std::string::npos)
-        cout << letter << " is in position " << found+1 << " in the word.\n" << scoreboard << endl;
+        
+        if (found != std::string::npos)
+        cout << letter << " is in position " << found+1 << " in the word.\n";
         else
         cout << letter << " is not in the word\n"; // not found
         if (found != std::string::npos)
-        scoreboard = scoreboard.replace(found, found+2, '_',  letter);
+        scoreboard = scoreboard.replace(found, 1,  letter);
         else
-        scoreboard = scoreboard;*/
+        scoreboard = scoreboard;
+    
     cout << scoreboard << endl;
         }
     cout << "you are out of guesses.\n";
+    
     return 0; 
 }
