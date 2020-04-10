8b1dc91f0471c94fabf9453dd15fe6388f767097
Jesse Turner <jesseturner@Jesses-MacBook-Pro.local>
2020-04-09 23:46:10 -0600

    Now has lives added, plus rudimentary spacers

--- hangman.cpp
+++ hangman.cpp
@@ -6,18 +6,24 @@ int main()
 { 
     string guessword; 
     string letter;
-  
+    int lives; 
+    string scoreboard; 
+
     cout << "Enter your hangman word: "; 
     cin >> guessword;
     cout << "The word is " << guessword.length() << " letters long.\n";
-    string scoreboard;
     scoreboard = string(guessword.length(), '_');
     cout << scoreboard << endl;
-    
-    for (int i = 0; i <= 3; i++) {
+    lives = 5;
+    cout << "You have " << lives << " lives to get the correct answer.\n";
+
+    while (lives > 0) {
+        int found;
+
+        cout << "----------------------------------------\n";
+
         cout << "Enter a letter to guess: ";
         cin >> letter;
-        int found;
         found = guessword.find(letter);
         
         if (found != std::string::npos)
@@ -27,9 +33,12 @@ int main()
         if (found != std::string::npos)
         scoreboard = scoreboard.replace(found, 1,  letter);
         else
-        scoreboard = scoreboard;
-    
-    cout << scoreboard << endl;
+        lives = lives-1;
+
+        cout << "You have " << lives << " lives left.\n";
+        cout << scoreboard << endl;
+
+        cout << "----------------------------------------\n\n";   
         }
     cout << "you are out of guesses.\n";
     
