68a56637be09bc8316d46277f299dd0628296cf3
Jesse Turner <jesseturner@Jesses-MacBook-Pro.local>
2020-06-10 11:59:49 -0600

    Somewhat fixed color change. Added green ball

--- screensaver.cpp
+++ screensaver.cpp
@@ -9,8 +9,8 @@ g++ -std=c++11 -o screensaver screensaver.cpp -I~/Desktop/SFML\ Tut/\ExternalLib
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include <ctime>
-#include <cmath>
+//#include <ctime>
+//#include <cmath>
 
 int main()
 {
@@ -20,24 +20,35 @@ int main()
 //Making variables
 	sf::CircleShape ball1(100.0, 100.0); //f is float (also i for int or u for undef)
 	sf::CircleShape ball2(100.0, 100.0);
+	sf::CircleShape ball3(100.0, 100.0);
 
 	ball2.setPosition(1300, 1300);
+	ball3.setPosition(650, 650);
 
 	float x_speed_1 = 200.f;
 	float y_speed_1 = 200.f;
 	float x_speed_2 = 200.f;
 	float y_speed_2 = 200.f;
+	float x_speed_3 = 200.f;
+	float y_speed_3 = 200.f;
 
 	sf::Clock move_clock;
 	sf::Clock color_clock;
 
 	float x_angle_1 = 1.2;
 	float x_angle_2 = 2;
+	float x_angle_3 = -0.5;
 	float y_angle_1 = 1;
 	float y_angle_2 = 1;
+	float y_angle_3 = 1.5;
 
-	sf::Color color_1(155, 0, 0);
-	sf::Color color_2(0, 0, 155);
+	sf::Color color_1(0, 0, 0);
+	sf::Color color_2(0, 0, 0);
+	sf::Color color_3(0, 0, 0);
+
+	bool countUp = true;
+	int colorCount = 0;
+	int colorCountDown = 255;
 
 //Starting animation
 	while (window.isOpen())
@@ -52,19 +63,74 @@ int main()
 
 		ball1.move(x_angle_1 * x_speed_1 * deltaTime, y_angle_1 * y_speed_1 * deltaTime);
 		ball2.move(x_angle_2 * -x_speed_2 * deltaTime, y_angle_2 * -y_speed_2 * deltaTime);
+		ball3.move(x_angle_3 * -x_speed_3 * deltaTime, y_angle_3 * -y_speed_3 * deltaTime);
 
 //Changing colors
-		color_1.r = colorTime % 255;
-		color_1.g = 50;
-		color_1.b = 50;
+		if (countUp) //red ball
+		{
+			colorCount = (colorTime % 255);
+			color_1.r = colorCount;
+			if (colorTime == 255)
+			{
+				colorCount = 0;
+				countUp = false;
+			}
 
-		color_2.r = 50;
-		color_2.g = 50;
-		color_2.b = colorTime % 255;
+		}
+		if (!countUp)
+		{
+			color_1.r = colorCountDown - (colorTime % 255);
+			if (colorCountDown - (colorTime % 255) == 1)
+			{
+				countUp = true;
+				colorCountDown = 255;
+			}
+		}
+		if (countUp) //blue ball
+		{
+			colorCount = (colorTime % 255);
+			color_2.b = colorCount;
+			if (colorTime == 255)
+			{
+				colorCount = 0;
+				countUp = false;
+			}
+
+		}
+		if (!countUp)
+		{
+			color_2.b = colorCountDown - (colorTime % 255);
+			if (colorCountDown - (colorTime % 255) == 1)
+			{
+				countUp = true;
+				colorCountDown = 255;
+			}
+		}
+		if (countUp) //green ball
+		{
+			colorCount = (colorTime % 255);
+			color_3.g = colorCount;
+			if (colorTime == 255)
+			{
+				colorCount = 0;
+				countUp = false;
+			}
+
+		}
+		if (!countUp)
+		{
+			color_3.g = colorCountDown - (colorTime % 255);
+			if (colorCountDown - (colorTime % 255) == 1)
+			{
+				countUp = true;
+				colorCountDown = 255;
+			}
+		}
 
 
 		ball1.setFillColor(color_1);
 		ball2.setFillColor(color_2);
+		ball3.setFillColor(color_3);
 
 
 //Bouncing on walls
@@ -86,12 +152,20 @@ int main()
 		if(ball2.getPosition().y >= 1300)
 			y_speed_2 = -y_speed_2;
 
-
+		if(ball3.getPosition().x <= 0)
+			x_speed_3 = -x_speed_3;
+		if(ball3.getPosition().x >= 1300)
+			x_speed_3 = -x_speed_3;
+		if(ball3.getPosition().y <= 0) 
+			y_speed_3 = -y_speed_3;
+		if(ball3.getPosition().y >= 1300)
+			y_speed_3 = -y_speed_3;
 
 
         window.clear();
         window.draw(ball1);
         window.draw(ball2);
+        window.draw(ball3);
         window.display();
     }
 }
