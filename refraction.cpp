bd56c51aacc96ebf3d5690ccc034f53e5d414ea7
Jesse Turner <jesseturner@Jesses-MacBook-Pro.local>
2020-07-21 13:35:07 -0600

    Reorganized. Recalculated dist_cross, but there is an error.

--- refraction.cpp
+++ refraction.cpp
@@ -37,14 +37,19 @@ int main()
 	ray_enter.setFillColor(sf::Color::Blue);
 	ray_cross.setFillColor(sf::Color::Blue);
 	ray_return.setFillColor(sf::Color::Blue);
+	ray_exit.setFillColor(sf::Color::Blue);
 	drop.setFillColor(sf::Color::Transparent);
 	drop.setOutlineThickness(10);
 
 	float n_one = 1;
 	float n_two;
-	float theta_one_rad;
-	float theta_two_rad;
-	float theta_two_deg;
+	float theta_one;
+	float theta_two;
+	float theta_three;
+	float theta_four;
+	float theta_five;
+	float theta_six;
+
 
 
 	std::cout << "Enter refraction index: " << std::endl;
@@ -68,45 +73,58 @@ int main()
 		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
 			enter_height++;
 
+
+
+		//set angles and distances
+		theta_one = asin( (750 - enter_height) / ( 350 ) ); //angle of entrance to center of circle
+		theta_two = asin( (n_one/n_two) * sin(theta_one) ); //angle between incedence and refracted line
+		theta_three = ( (M_PI/2) - (theta_one + theta_two));
+		theta_four = ( theta_one + theta_three );
+		theta_five = ((M_PI/2) - (theta_four/2));
+		theta_six = asin( (n_one/n_two) * sin(theta_four) );
+
+		float x_dist_to = ( ( -cos( theta_one ) * 350 ) + 750); 
+		float y_dist_to = ( ( sin( theta_one ) * 350 ) - 750); //for reflection
+
+		float dist_cross = std::abs( 2 * ( 350 * cos( theta_three ) ) ); //calculating the chord distance
+
+		float cross_x = (cos( theta_two ) * dist_cross);
+		float cross_y = -(sin( theta_two ) * dist_cross);
+
+		float dist_return = 2 * ( 350 * sin( ( theta_four ) / 2 ) );
+		float x_dist_exit = ((x_dist_to + cross_x) - (sin(theta_four) * 350));
+		float y_dist_exit = ((y_dist_to + cross_y) + (cos(theta_four) * 350));
+
+
+
+		
 		ray_enter.setPosition(0, enter_height);
+		ray_enter.setSize(sf::Vector2f(x_dist_to, 10.0f));
 
+		float theta_two_deg = rad_to_deg(theta_two);
 
-		//angle from the center of the circle
-		theta_one_rad = asin( (750 - enter_height) / ( 350 ) );
-
-		//distance before hitting the circle
-		float dist_to = ( ( -cos( theta_one_rad ) * 350 ) + 750); 
-		float y_dist_to = ( ( sin( theta_one_rad ) * 350 ) - 750); //for reflection
-		ray_enter.setSize(sf::Vector2f(dist_to, 10.0f));
-
-		//refraction after entering
-		theta_two_rad = asin( (n_one/n_two) * sin(theta_one_rad) ); //sin needs rad
-		theta_two_deg = rad_to_deg(theta_two_rad);
-
-
-		//ray crossing the drop, position, size, and angle
-		ray_cross.setPosition(dist_to, enter_height); 
+		ray_cross.setPosition(x_dist_to, enter_height); 
 		ray_cross.setRotation(theta_two_deg);
-		float dist_cross = 2 * ( 350 * sin( ( 3.1416 - theta_one_rad ) / 2 ) ); //calculating the chord distance
 		ray_cross.setSize(sf::Vector2f(dist_cross, 10.0f));
 
-		std::cout << theta_two_deg << std::endl;
+		float theta_three_deg = rad_to_deg(theta_three);
+		float theta_four_deg = rad_to_deg(theta_four);
 
-		//ray returning across the drop
-		float cross_x = (cos( theta_two_rad ) * dist_cross);
-		float cross_y = -(sin( theta_two_rad ) * dist_cross);
-		ray_return.setPosition((cross_x + dist_to), -(cross_y + y_dist_to));
-		ray_return.setRotation(180 - theta_two_deg);
 
-		float theta_three_rad = 3.1416 - ( 2 * (theta_two_rad - theta_one_rad) ); //not correct for theta_three
-		float dist_return = 2 * ( 350 * sin( ( theta_three_rad ) / 2 ) );
+		ray_return.setPosition((cross_x + x_dist_to), -(cross_y + y_dist_to));
+		ray_return.setRotation( 180 - theta_two_deg);
 		ray_return.setSize(sf::Vector2f( dist_return, 10.0f));
+
+		ray_exit.setPosition(x_dist_exit, y_dist_exit);
+		ray_exit.setRotation(180);
+		ray_exit.setSize(sf::Vector2f( 400.f, 10.f ));
 
 
 	    window.clear();
         window.draw(ray_enter);
         window.draw(ray_cross);
         window.draw(ray_return);
+        window.draw(ray_exit);
         window.draw(drop);
         window.display();
     }
